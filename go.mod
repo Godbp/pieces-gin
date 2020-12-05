5e8904604f8cc4357ec9846c6fa2aeed34146b4c
彭博 <pengbo@dustess.com>
2020-12-05 11:09:44 +0800

    feat: 🎸 添加中间件跟踪ID

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/jinzhu/gorm v1.9.12
+	github.com/satori/go.uuid v1.2.0
 	github.com/sirupsen/logrus v1.6.0
 	github.com/spf13/viper v1.6.3
 )
--- middleware/attestation.go
+++ middleware/attestation.go
@@ -1,10 +1,16 @@
 package middleware
 
 import "github.com/gin-gonic/gin"
+import "github.com/satori/go.uuid"
 
 func Token() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		// 设置请求跟踪ID
+		reqID := c.Writer.Header().Get("X-Request-Id")
+		if len(reqID) == 0 {
+			reqID = uuid.NewV4().String()
+			c.Writer.Header().Set("X-Request-Id", reqID)
+		}
 		c.Next()
 	}
-
 }
