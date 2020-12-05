f356b1fa46953999c4ebc3a1402425d844d8cd34
彭博 <pengbo@dustess.com>
2020-12-05 15:22:16 +0800

    feat: 🎸 修改中间件

--- middleware/log.go
+++ middleware/log.go
@@ -3,20 +3,28 @@ package middleware
 import (
 	"bytes"
 	"github.com/gin-gonic/gin"
+	uuid "github.com/satori/go.uuid"
 	"github.com/sirupsen/logrus"
 	"io/ioutil"
 	"net/http"
 )
 
+// Params 此中间件放在
 func Params() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		if c.Request.Method == http.MethodGet{
-			logrus.Infof("接口：【%v】 method：【%v】 params： 【%v】",c.Request.URL, c.Request.Method, c.Request.PostForm)
-		} else {
-			data, _ := ioutil.ReadAll(c.Request.Body)
-			logrus.Infof("接口：【%v】 method：【%v】 params： 【%v】",c.Request.URL, c.Request.Method, string(data))
-			c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(data))
+	return func(ctx *gin.Context) {
+		// 设置请求跟踪ID
+		reqID := ctx.Writer.Header().Get("X-Request-Id")
+		if len(reqID) == 0 {
+			reqID = uuid.NewV4().String()
+			ctx.Writer.Header().Set("X-Request-Id", reqID)
 		}
-		c.Next()
+		if ctx.Request.Method == http.MethodGet {
+			logrus.Infof("接口:[%s] method:[%s] params:[%s]", ctx.Request.URL, ctx.Request.Method, ctx.Request.PostForm)
+		} else {
+			data, _ := ioutil.ReadAll(ctx.Request.Body)
+			logrus.Infof("接口:[%s] method:[%s] params:[%s]", ctx.Request.URL, ctx.Request.Method, string(data))
+			ctx.Request.Body = ioutil.NopCloser(bytes.NewBuffer(data))
+		}
+		ctx.Next()
 	}
 }
